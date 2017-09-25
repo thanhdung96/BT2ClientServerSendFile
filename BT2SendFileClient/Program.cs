7425225adaee978643b7866aeff5f928383afb92
thanhdung96 <14dh110224@st.huflit.edu.vn>
2017-09-25 11:57:37 +0700

    Init Project

---
+++ BT2SendFileClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BT2SendFileClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ BT2SendFileServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BT2SendFileServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
