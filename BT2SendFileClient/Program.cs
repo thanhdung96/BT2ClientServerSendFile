ced6a70dd6670dc462168a5a89995f5c0971c2cb
thanhdung96 <14dh110224@st.huflit.edu.vn>
2017-09-28 08:23:48 +0700

    finished client-side. fixing may required.

--- BT2SendFileClient/Program.cs
+++ BT2SendFileClient/Program.cs
@@ -1,25 +1,12 @@
-﻿using System;
-using System.Text;
-using System.Net;
-using System.Net.Sockets;
-
-namespace BT2SendFileClient
+﻿namespace BT2SendFileClient
 {
 	class Program
 	{
-		private Socket clientSock;
-		byte[] dataToSend;
-		byte[] dataToReceive;
-
-		public Program()
-		{
-			clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);	//create socket using IPv4, type stream, TCP
-			clientSock.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1724));
-			clientSock.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1724));
-		}
-
+		private static Client client;
 		static void Main(string[] args)
 		{
+			client = new Client();
+			client.receiveFile();
 		}
 	}
 }
--- BT2SendFileServer/Server.cs
+++ BT2SendFileServer/Server.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Text;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
-using System.IO;
+using System.Text;
 
 /**
  * Signals send from client:
@@ -15,6 +15,8 @@ using System.IO;
 /**
  * Signals send from server:
  *	1 (0000 0001)		server acknowledged signal from client
+ *	2 (0000 0010)		server inform about finished sending file
+ *	3 (0000 0011)		server inform about sending next fragment
 **/
 namespace BT2SendFileServer
 {
@@ -23,7 +25,7 @@ namespace BT2SendFileServer
 		private Socket serverSock;		//socket bind to server
 		private Socket clientSock;		//socket represent client
 		private byte[] dataToSend;
-		private byte[] dataToReceive;
+		private byte[] dataReceive;
 		private byte signalFromDataReceived;
 
 		public Server()
@@ -35,8 +37,8 @@ namespace BT2SendFileServer
 			serverSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);	//create socket using IPv4, type stream, TCP
 			serverSock.Bind(new IPEndPoint(IPAddress.Any, 1724));	//listen to any network interface at port 1724
 			//init data stream
-			dataToSend = new byte[1024];		//data send to client
-			dataToReceive = new byte[1];		//signal receive from client 1 byte
+			dataToSend = new byte[4096];		//data send to client, 4Kb buffer
+			dataReceive = new byte[1];		//signal receive from client 1 byte of value 1
 		}
 
 		public void startListen()
@@ -48,15 +50,28 @@ namespace BT2SendFileServer
 			Console.WriteLine("Server accepted a client...");
 
 			//receive first signal from client
-			clientSock.Receive(dataToReceive);
-			signalFromDataReceived = dataToReceive[0];		//get first byte from data stream
+			clientSock.Receive(dataReceive);
+			signalFromDataReceived = dataReceive[0];		//get first byte from data stream
 
 			if (signalFromDataReceived == 1)	//if client is ready
 			{
-				clientSock.Send(new byte[1]{1});		//send 1 byte of value 1 to client to ACK
+				clientSock.Send(new byte[1] { 1 });		//send 1 byte of value 1 to client to ACK
 				Console.WriteLine("Server sending file to client...");
 				sendFile();		//begin transferring file
 			}
+
+			clientSock.Receive(dataReceive);
+			signalFromDataReceived = dataReceive[0];
+			if (signalFromDataReceived == 8)	//if client wants to disconnect
+			{
+				disconnectClient();
+			}
+		}
+
+		private void disconnectClient()
+		{
+			clientSock.Close();
+			Console.WriteLine("Client disconnected from server...");
 		}
 
 		private void sendFile()
@@ -68,17 +83,23 @@ namespace BT2SendFileServer
 			{
 				string line;
 				// Read and display lines from the file until the end of the file is reached. 
+				clientSock.Send(new byte[1] { 3 });		//inform client about sending next file fragment 1 byte of value 3
 				while ((line = sr.ReadLine()) != null)
 				{
-					clientSock.Receive(dataToReceive);
-					signalFromDataReceived = dataToReceive[0];		//get first byte from data stream
 
-					if (signalFromDataReceived == 0)
+					dataToSend = Encoding.ASCII.GetBytes(line);		//encode string to bytes array
+					clientSock.Send(dataToSend, line.Length, SocketFlags.None);		//sned to client
+
+					clientSock.Receive(dataReceive);
+					signalFromDataReceived = dataReceive[0];		//get first byte from data stream
+
+					if (signalFromDataReceived != 0)		//if client not ACK to fragment sent
 					{
-						dataToSend = Encoding.ASCII.GetBytes(line);
-						clientSock.Send(dataToSend, line.Length, SocketFlags.None);
+						break;		//stop sending
 					}
+
 				}
+				clientSock.Send(new byte[1] { 2 });		//inform client about finished sending file 1 byte of value 2
 			}
 		}
 	}
